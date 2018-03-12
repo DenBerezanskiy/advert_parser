9a1cac3410d2d96bdb3c723a23cd6d86f644de8a
Denis <dstiven@i.ua>
2018-03-12 08:46:58 +0100

    1.Added html parser 2.Added advert entity

---
+++ src/main/java/ua/dp/advert_parser/core/Parser.java
@@ -0,0 +1,97 @@
+package ua.dp.advert_parser.core;
+
+import org.ccil.cowan.tagsoup.jaxp.SAXParserImpl;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import ua.dp.advert_parser.entity.Advert;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Denis Berezanskiy on 12.03.2018.
+ */
+public class Parser {
+        private Advert advert;
+
+        /**
+         * Method below is parsing html page with list of adverts .
+         * @param url - prepared url with selected parameters of search
+         * @return List of adverts links
+         */
+        public List parsePage(String url)
+        {
+            List<String> links = new ArrayList<>();
+            try
+            {
+                SAXParserImpl.newInstance(null).parse(
+                        new URL(url).openConnection().getInputStream(),
+                        new DefaultHandler()
+                        {
+                            public void startElement(String uri, String localName,
+                                                     String name, Attributes a)
+                            {
+                                if (name.equalsIgnoreCase("a") && a != null && a.getValue("class") != null &&
+                                        a.getValue("class").contains("detailsLink"))
+                                    links.add(a.getValue("href"));
+                            }
+                        }
+                );
+
+            }
+            catch (SAXException | IOException e)
+            {
+                e.printStackTrace();
+            }
+            return links;
+        }
+
+        /**
+         *  Following method is obtain advert parameters like title , price ,description and link
+         * @param link - reference of Advert
+         * @return an advert with inserted parameters in it
+         */
+        public Advert parseAdvert(String link)
+        {
+            advert = new Advert();
+            advert.setUrl(link);
+            try
+            {
+                Document document = Jsoup.connect(link).get();
+                //Getting title
+                Elements elements = document.getElementsByAttributeValue("class" , "offer-titlebox");
+                //TODO : unhardcode
+                advert.setTitle(elements.get(0).child(0).text());
+                //Getting price
+                elements = document.getElementsByAttributeValue("class", "price-label");
+                advert.setPrice(elements.text());
+                //Getting description
+                elements = document.getElementsByAttributeValue("class" , "clr");
+                //TODO : unhardcode
+                advert.setDescription(elements.get(1).getElementsByAttributeValue("class" , "pding10 lheight20 large").text());
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            if(advert == null)
+            {
+                System.out.println("something goes wrong!!!");
+            }
+            return advert;
+        }
+
+        public void setAdvert(Advert Advert) {
+            advert = Advert;
+        }
+
+        public Advert getAdvert() {
+            return advert;
+        }
+}
---
+++ src/main/java/ua/dp/advert_parser/entity/Advert.java
@@ -0,0 +1,68 @@
+package ua.dp.advert_parser.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Denis Berezanskiy on 12.03.2018.
+ */
+public class Advert
+{
+    private long id;
+    private String url;
+    private String title;
+    //price is String variable because it displaying like a String value in html page.
+    private String price;
+    private String description;
+
+    public Advert() {
+    }
+
+    public Advert(String url, String title, String price, String description) {
+        this.url = url;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Advert{" +
+                "url='" + url + '\'' +
+                ",\n title='" + title + '\'' +
+                ",\n price='" + price + '\'' +
+                ",\n description='" + description + '\'' +
+                '}';
+    }
+}
